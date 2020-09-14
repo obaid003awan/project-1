d3b1bdb28f774c2c6a8e6437ccec35fb11b69f14
obaid12awan <obaidawan11253@gmail.com>
2020-09-14 14:51:48 +0500

    first commit

---
+++ Controllers/ItemList.cs
@@ -0,0 +1,10 @@
+﻿namespace obaid_ayub.Controllers
+{
+    internal class ItemList
+    {
+        public int ID { get; internal set; }
+        public string Name { get; internal set; }
+        public string Category { get; internal set; }
+        public int Price { get; internal set; }
+    }
+}
---
+++ Controllers/itemController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace obaid_ayub.Controllers
+{
+    public class itemController : Controller
+    {
+        // GET: item
+        public ActionResult Index()
+        {
+            ViewBag.ItemList = "Computer Shop Item List Page";
+
+            List<ItemList> IList = new List<ItemList>();
+            {
+                new ItemList { ID = 1, Name = "iPhone", Category = "Mobile", Price = 2393 };
+                new ItemList { ID = 2, Name = "HardDisk", Category = "Computer", Price = 9399 };
+                new ItemList { ID = 3, Name = "Mouse", Category = "Computer", Price = 120 };
+                new ItemList { ID = 4, Name = "Samsung Note3", Category = "Mobile", Price = 9348 };
+            }
+            return View(IList);
+        }
+    }
+}
